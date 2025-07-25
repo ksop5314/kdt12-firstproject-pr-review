5b89ef474ba9bd697c029f6739064fcdf6a8b360
doono <ksop6580@naver.com>
2025-07-25 17:50:17 +0900

    Fix: sub page 댓글 창 수정
    - 댓글창 focus 시 버튼들 보여지게 수정. 취소 버튼 기능 구현, 댓글달기 버튼 input 칸에 글자 있으면 활성화  (아직 댓글 기능은 미완성)

--- js/reply-box.js
+++ js/reply-box.js
@@ -6,9 +6,24 @@ const submitBtn = document.getElementById("submitBtn");
 
 replyInput.addEventListener('focus',  () => {
     replyInput.classList.add("active");
+    replyActions.style.display = "flex";
+    replyInput.placeholder = "";
 });
 
 cancelBtn.addEventListener("click", () => {
    replyInput.classList.remove("active");
    replyInput.value = "";
+   replyActions.style.display = "none";
+    replyInput.placeholder = "댓글 추가...";
+});
+
+replyInput.addEventListener("input", () => {
+
+    if (replyInput.value.trim().length > 0) {
+        submitBtn.classList.add("enabled");
+        // submitBtn.style.cursor = "pointer";
+    } else {
+        submitBtn.classList.remove("enabled");
+    }
+
 });
