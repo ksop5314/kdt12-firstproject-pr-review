cf48b1845c09753d271c59d6e549529569446ce4
doono <ksop6580@naver.com>
2025-07-28 16:38:19 +0900

    Feat: 댓글창 수정 및 추가
    - 달려있는 댓글에 대댓글 UI 완료
    - 댓글, 대댓글 기능 미구현
    - 댓글 좋아요 기능 구현 완료

--- js/like-dislike-btn.js
+++ js/like-dislike-btn.js
@@ -7,7 +7,7 @@ likeBtn.addEventListener("click", () => {
     likeBtn.classList.toggle("fa-regular", !isLiked);
     likeBtn.classList.toggle("fa-solid", isLiked);
 
-    // 싫어요는 자동 해제
+    // 좋아요와 싫어요 중 하나만 선택 가능
     if (isLiked) {
         dislikeBtn.classList.remove("disliked", "fa-solid");
         dislikeBtn.classList.add("fa-regular");
@@ -19,9 +19,130 @@ dislikeBtn.addEventListener("click", () => {
     dislikeBtn.classList.toggle("fa-regular", !isDisliked);
     dislikeBtn.classList.toggle("fa-solid", isDisliked);
 
-    // 좋아요는 자동 해제
+    // 좋아요와 싫어요 중 하나만 선택 가능
     if (isDisliked) {
         likeBtn.classList.remove("liked", "fa-solid");
         likeBtn.classList.add("fa-regular");
+    }
+});
+
+// 댓글 좋아요 싫어요
+const replyLikeBtn = document.getElementById("replyLikeBtn");
+const replyDislikeBtn = document.getElementById("replyDislikeBtn");
+const replyLikeCnt = document.getElementById("replyLikeCnt");
+
+replyLikeBtn.addEventListener("click", () => {
+    const isReplyLiked = replyLikeBtn.classList.toggle("liked");
+    replyLikeBtn.classList.toggle("fa-regular", !isReplyLiked);
+    replyLikeBtn.classList.toggle("fa-solid", isReplyLiked);
+
+    let test = replyLikeCnt.innerText;
+    let cnt = parseInt(test === "" ? 0 : test);
+
+    if (isReplyLiked) {
+        replyDislikeBtn.classList.remove("disliked", "fa-solid");
+        replyDislikeBtn.classList.add("fa-regular");
+
+        cnt += 1;
+        replyLikeCnt.innerText = cnt.toString();
+        console.log(cnt);
+    }
+
+    if (!isReplyLiked) {
+        cnt -= 1;
+        if (cnt === 0 ){
+            replyLikeCnt.innerText = "";
+            console.log(cnt);
+        } else {
+            replyLikeCnt.innerText = cnt.toString();
+        }
+    }
+});
+
+replyDislikeBtn.addEventListener("click", () => {
+    const isReplyDisliked = replyDislikeBtn.classList.toggle("disliked");
+    replyDislikeBtn.classList.toggle("fa-regular", !isReplyDisliked);
+    replyDislikeBtn.classList.toggle("fa-solid", isReplyDisliked);
+
+    let test = replyLikeCnt.innerText;
+    let cnt = parseInt(test === "" ? 0 : test);
+
+    if (isReplyDisliked) {
+        const isReplyLiked = replyLikeBtn.classList.toggle("liked");
+        replyLikeBtn.classList.remove("liked", "fa-solid");
+        replyLikeBtn.classList.add("fa-regular");
+
+        console.log(isReplyLiked);
+        console.log(cnt);
+
+        if (!isReplyLiked) {
+            cnt -= 1;
+            if (cnt === 0) {
+                replyLikeCnt.innerText = "";
+            } else {
+                replyLikeCnt.innerText = cnt.toString();
+            }
+        }
+    }
+
+});
+
+// user2 좋아요 싫어요
+const replyLikeBtn2 = document.getElementById("replyLikeBtn2");
+const replyDislikeBtn2 = document.getElementById("replyDislikeBtn2");
+const replyLikeCnt2 = document.getElementById("replyLikeCnt2");
+
+replyLikeBtn2.addEventListener("click", () => {
+    const isReplyLiked = replyLikeBtn2.classList.toggle("liked");
+    replyLikeBtn2.classList.toggle("fa-regular", !isReplyLiked);
+    replyLikeBtn2.classList.toggle("fa-solid", isReplyLiked);
+
+    let test = replyLikeCnt2.innerText;
+    let cnt = parseInt(test === "" ? 0 : test);
+
+    if (isReplyLiked) {
+        replyDislikeBtn2.classList.remove("disliked", "fa-solid");
+        replyDislikeBtn2.classList.add("fa-regular");
+
+        cnt += 1;
+        replyLikeCnt2.innerText = cnt.toString();
+        console.log(cnt);
+    }
+
+    if (!isReplyLiked) {
+        cnt -= 1;
+        if (cnt === 0 ){
+            replyLikeCnt2.innerText = "";
+            console.log(cnt);
+        } else {
+            replyLikeCnt2.innerText = cnt.toString();
+        }
+    }
+});
+
+replyDislikeBtn2.addEventListener("click", () => {
+    const isReplyDisliked = replyDislikeBtn2.classList.toggle("disliked");
+    replyDislikeBtn2.classList.toggle("fa-regular", !isReplyDisliked);
+    replyDislikeBtn2.classList.toggle("fa-solid", isReplyDisliked);
+
+    let test = replyLikeCnt2.innerText;
+    let cnt = parseInt(test === "" ? 0 : test);
+
+    if (isReplyDisliked) {
+        const isReplyLiked = replyLikeBtn2.classList.toggle("liked");
+        replyLikeBtn2.classList.remove("liked", "fa-solid");
+        replyLikeBtn2.classList.add("fa-regular");
+
+        console.log(isReplyLiked);
+        console.log(cnt);
+
+        if (!isReplyLiked) {
+            cnt -= 1;
+            if (cnt === 0) {
+                replyLikeCnt2.innerText = "";
+            } else {
+                replyLikeCnt2.innerText = cnt.toString();
+            }
+        }
     }
 });
--- js/reply-box.js
+++ js/reply-box.js
@@ -3,10 +3,12 @@ const replyInput = document.getElementById("replyInput");
 const replyActions = document.getElementById("replyActions");
 const cancelBtn = document.getElementById("cancelBtn");
 const submitBtn = document.getElementById("submitBtn");
+const replyText = document.getElementById("replyText");
 
 replyInput.addEventListener('focus',  () => {
     replyInput.classList.add("active");
     replyActions.style.display = "flex";
+    replyText.style.marginBottom = "30px";
     replyInput.placeholder = "";
 });
 
@@ -14,16 +16,63 @@ cancelBtn.addEventListener("click", () => {
    replyInput.classList.remove("active");
    replyInput.value = "";
    replyActions.style.display = "none";
-    replyInput.placeholder = "댓글 추가...";
+   replyText.style.marginBottom = "0";
+   replyInput.placeholder = "댓글 추가...";
 });
 
 replyInput.addEventListener("input", () => {
 
     if (replyInput.value.trim().length > 0) {
         submitBtn.classList.add("enabled");
-        // submitBtn.style.cursor = "pointer";
     } else {
         submitBtn.classList.remove("enabled");
     }
-
 });
+
+
+// 댓글에 댓글 달기 버튼
+const reReplyBtn = document.getElementById("replyBtn");
+const reReplyInputBox = document.getElementById("reReplyInputBox");
+const reReplyCancelBtn = document.getElementById("reCancelBtn");
+const reReplyInput = document.getElementById("reReplyInput");
+const reReplySubmitBtn = document.getElementById("reSubmitBtn");
+
+reReplyBtn.addEventListener("click", () => {
+    reReplyInputBox.style.display = "flex";
+});
+
+reReplyCancelBtn.addEventListener("click", () => {
+    reReplyInputBox.style.display = "none";
+});
+
+reReplyInput.addEventListener("input", () => {
+    if (reReplyInput.value.trim().length > 0) {
+        reReplySubmitBtn.classList.add("enabled");
+    } else {
+        reReplySubmitBtn.classList.remove("enabled");
+    }
+});
+
+// user2 댓글에 댓글 달기 버튼
+const reReplyBtn2 = document.getElementById("replyBtn2");
+const reReplyInputBox2 = document.getElementById("reReplyInputBox2");
+const reReplyCancelBtn2 = document.getElementById("reCancelBtn2");
+const reReplyInput2 = document.getElementById("reReplyInput2");
+const reReplySubmitBtn2 = document.getElementById("reSubmitBtn2");
+
+reReplyBtn2.addEventListener("click", () => {
+    reReplyInputBox2.style.display = "flex";
+});
+
+reReplyCancelBtn2.addEventListener("click", () => {
+    reReplyInputBox2.style.display = "none";
+});
+
+reReplyInput2.addEventListener("input", () => {
+    if (reReplyInput2.value.trim().length > 0) {
+        reReplySubmitBtn2.classList.add("enabled");
+    } else {
+        reReplySubmitBtn2.classList.remove("enabled");
+    }
+});
+
