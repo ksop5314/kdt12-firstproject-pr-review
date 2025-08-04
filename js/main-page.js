87a915751be5e5ab37fbe13189c88f2bbeddb391
ksop5314 <134660408+ksop5314@users.noreply.github.com>
2025-08-04 18:16:52 +0900

    FixUpdate main-page.js
    -

--- js/main-page.js
+++ js/main-page.js
@@ -5,7 +5,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/ZPQlKwFjKyI/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDyHjkPSEs-2c0WNXRKSFAK0u5FIw",
             title: "와 미친.. 강등권과 3점차.. 김판곤 끝난 것 같다 [울산vs대전]",
             channel: "달수네라이브",
-            channelLogo: "/first_project/img/youtube-logo-1.png",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-1.png",
             view: "조회수 4만회 ㆍ ",
             date: "10시간 전"
         },
@@ -13,7 +13,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/QgoGQux_M5U/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCrUFTnDj0B3URx-HNnTJZRro42Rg",
             title: "[EN] 행운의 77회는 핑계고 | EP.77",
             channel: "뜬뜬 DdeunDdeun",
-            channelLogo: "/first_project/img/youtube-logo-2.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-2.jpg",
             view: "조회수 264만회 ㆍ ",
             date: "2개월 전"
         },
@@ -21,7 +21,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/MgKoBlm35tY/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLB10KjPOnXpMM-QG0crxwjTJ-p8sA",
             title: "[2025 K리그1] 22R 전북 vs 강원 풀 하이라이트",
             channel: "쿠팡플레이 스포츠",
-            channelLogo: "/first_project/img/youtube-logo-3.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-3.jpg",
             view: "조회수 3만회 ㆍ ",
             date: "7시간 전"
         },
@@ -29,7 +29,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/qB5aE24YuOQ/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCQYt1IeoqYBie9vd5_mVU7tu5ZbA",
             title: "난 월 1000 벌어 너는?",
             channel: "웃기시네",
-            channelLogo: "/first_project/img/youtube-logo-4.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-4.jpg",
             view: "조회수 1.4만회 ㆍ ",
             date: "1시간 전"
         },
@@ -37,7 +37,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/bJfbPWEMj_c/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDrd2qdeJcP_LbzMiY_4Ere9thKzg",
             title: "스프링 제대로 공부했는지 5분안에 확인하는 방법",
             channel: "백기선",
-            channelLogo: "/first_project/img/youtube-logo-5.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-5.jpg",
             view: "조회수 16만회 ㆍ ",
             date: "3년 전"
         },
@@ -45,7 +45,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/5HGVrMUQCRo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCg_-IFj-DgS3ZmsAHZjQEPqjxXqg",
             title: "후회하더라도 피하는 것이 좋다",
             channel: "정서불안 김햄찌",
-            channelLogo: "/first_project/img/youtube-logo-6.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-6.jpg",
             view: "조회수 47만회 ㆍ ",
             date: "2일 전"
         },
@@ -53,7 +53,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/whixTynwRTo/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCl1bBIpE3xRsXgGBH5dp0WcjiOoQ",
             title: "위로가 필요한 힘들고 어려운 시간을 지나고 있다면 - 김창옥 [정기강연 ep.339]",
             channel: "김창옥TV",
-            channelLogo: "/first_project/img/youtube-logo-7.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-7.jpg",
             view: "조회수 45만회 ㆍ ",
             date: "2주 전"
         },
@@ -61,7 +61,7 @@ videoListData = {
             img: "https://i.ytimg.com/vi/v3E5c_D-Ee4/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDK9lExLupm_3BTHWmT9KyXkwaELg",
             title: "여객기에 바짝 다가온 폭격기...충돌 직전 회피 / SBS",
             channel: "SBS 뉴스",
-            channelLogo: "/first_project/img/youtube-logo-8.jpg",
+            channelLogo: "https://ksop5314.github.io/kdt12-firstproject-pr-review/img/youtube-logo-8.jpg",
             view: "조회수 27만회 ㆍ ",
             date: "10일 전"
         }
